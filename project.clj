0eeb193ba998e8c3f4c0c8fa93338bec8b837d87
Kyle Cordes <kyle@kylecordes.com>
2012-09-28 22:12:59 -0500

    Switch to Lein2, and the CCW Lein plugin. 'lein eclipse' is no more.

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
-(defproject ghijira "1.0.0-SNAPSHOT"
-  :description "Export GitHub Issues to JIRA-compatible CSV format"
+(defproject ghijira/ghijira "1.0.0-SNAPSHOT" 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [tentacles "0.2.0-beta1"]
                  [org.clojure/data.csv "0.1.2"]
                  [clj-time "0.4.2"]]
-
-  :dev-dependencies [
-        [lein-eclipse "1.0.0"]
-  ]
   :main ghijira.core
-)
+  :min-lein-version "2.0.0"
+  :plugins [[lein-eclipse "1.0.0"]]
+  :description "Export GitHub Issues to JIRA-compatible CSV format")
