b6c9d80a358799314c66a0d46e393d46954c7599
Kyle Cordes <kyle@kylecordes.com>
2013-05-01 20:21:23 -0500

    Update Clojure version, library versions, project version.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ghijira/ghijira "1.0.0-SNAPSHOT" 
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [tentacles "0.2.0-beta1"]
+(defproject ghijira/ghijira "1.1" 
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [tentacles "0.2.3"]
                  [org.clojure/data.csv "0.1.2"]
-                 [clj-time "0.4.2"]
-                 [org.clojure/core.memoize "0.5.2"]]
+                 [clj-time "0.5.0"]
+                 [org.clojure/core.memoize "0.5.3"]]
   :main ghijira.core
   :min-lein-version "2.0.0"
   :plugins [[lein-eclipse "1.0.0"]]
